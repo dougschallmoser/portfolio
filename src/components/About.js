13d2585e00a270cc759aa179f1e336a1338be9bb
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-25 13:42:59 -0800

    update components and build

--- src/components/About.js
+++ src/components/About.js
@@ -5,15 +5,16 @@ const About = () => {
   return (
     <section className="about">
       <div className="about-content">
-      <div id="skills">
-        MY STORY
-      <hr />
-      </div>
-        Hi! My name is Cindy Kim and I’ve graduated from Flation School in March 2016 as a Web Developer. Let me start with a small anecdote as to how I got here. 
+        <div id="about-mystory">
+          CIVIL ENGINEER TO SOFTWARE DEVELOPER
+        </div>
+        <hr />
+        <p>Hi! My name is Cindy Kim and I’ve graduated from Flation School in March 2016 as a Web Developer. Let me start with a small anecdote as to how I got here. 
         My first experience with coding was through a popular blog called Xanga around 2002. This blog had limited themes to choose from so you needed to learn html in order to truly personalize it. It’s no longer around and I wonder why… I didn’t know that this would trigger something later on in my future but it did!
         At the time I was really passionate for Fashion so I attended Fashion Institute of Technology for Fashion Merchandising and Management (currently split between Fashion Business Management and Production Management). I obtained an undergraduate Bachelors of Science degree and started my career in production/product development.
         I enjoyed it very much because it gave me a balance between being analytical and creative. It helped me be creative by having influence on approvals as concept of design was being executed in production. It was analytical because I needed to understand which factories had certain capacities that would allow us to reach shipping dates and cost margins.
-      <Skills />
+        </p>
+        <Skills />
       </div>
     </section>
   )
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 const Splash = () => {
 
+  const overlay = React.createRef();
+
   const handleButtonHover = () => {
     if (overlay.current.className === "splash-overlay-hover") {
       overlay.current.className = "splash-overlay"
@@ -10,22 +12,19 @@ const Splash = () => {
     }
   }
 
-  const overlay = React.createRef();
-
   return (
     <section className="splash">
       <div className="splash-intro">
         <div className="splash-name">I am Doug Schallmoser.</div>
         <div className="splash-bio">
           I'm a software developer living in Bellingham, WA.
-          I love the challenge that coding brings and the ability to make people's lives better with it.
+          I love the challenge that coding brings and the subsequent potential it provides to make lives better.
           <div className="splash-button" onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>
             <a href="#projects">View Projects</a>
           </div>
         </div>
       </div>
-      <div ref={overlay} className="splash-overlay">
-      </div>
+      <div ref={overlay} className="splash-overlay"></div>
     </section>
   )
 }
