9b7555eae72d059bccd846361e296d8b1da5c4b8
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-27 14:17:20 -0800

    update bio

--- src/components/About.js
+++ src/components/About.js
@@ -10,10 +10,17 @@ const About = () => {
         <div id="about-mystory">
           CIVIL ENGINEER TO SOFTWARE DEVELOPER
         </div>
-        <p>Hi! My name is Cindy Kim and I’ve graduated from Flation School in March 2016 as a Web Developer. Let me start with a small anecdote as to how I got here. 
-        My first experience with coding was through a popular blog called Xanga around 2002. This blog had limited themes to choose from so you needed to learn html in order to truly personalize it. It’s no longer around and I wonder why… I didn’t know that this would trigger something later on in my future but it did!
-        At the time I was really passionate for Fashion so I attended Fashion Institute of Technology for Fashion Merchandising and Management (currently split between Fashion Business Management and Production Management). I obtained an undergraduate Bachelors of Science degree and started my career in production/product development.
-        I enjoyed it very much because it gave me a balance between being analytical and creative. It helped me be creative by having influence on approvals as concept of design was being executed in production. It was analytical because I needed to understand which factories had certain capacities that would allow us to reach shipping dates and cost margins.
+        <p>
+          Hello! My name is Doug Schallmoser and I am a software developer living in Bellingham, WA. I love the challenge that coding brings and the subsequent potential it provides to make lives better. My first experience with coding came in 2019 while I was studying for the PE (Professional Engineering) exam. I started watching lectures from Harvard's computer science course "CS50" in between studying for the exam. Before I knew it I was hooked and wanted to pursue coding further.  
+        </p>
+        <p>
+        I graduated from Florida Gulf Coast University in 2012 with Bachelor of Science degrees in Civil Engineering and Environmental Engineering. I then worked for the Florida Department of Transportation as a civil engineer from late 2013 until early 2020. I learned so many valuable things during this time, from technical engineering practices to project management skills. In January 2020, I became a licensed Professional Engineer in Florida. I then quit my job and my wife and I relocated to Washington state.
+        </p>
+        <p>
+          After we settled in to our new place, I knew I wanted to switch careers. I enrolled in Flatiron School's Software Engineering program and formally began in July. I graduated the program in December 2020 having spent 1200+ hours learning and coding during the five month long program. I built several projects to showcase my skillset which are provided on this site.
+        </p>
+        <p>
+        I am continuing my personal education efforts by building new projects and learning new things. I believe my passion for coding combined with my technical background in engineering gives me a unique skillset and perspecive on problem solving in this vast world of technology and programming.
         </p>
         <Skills />
       </div>
