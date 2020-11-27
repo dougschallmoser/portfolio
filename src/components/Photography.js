f300f3553ffdf897426f89b0ed5c05103cbcb7fa
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-26 23:26:18 -0800

    comment out useEffect to cancel errors out

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const Photography = () => {
 
-  useEffect(() => {
-    fetch('https://instagram.com/illbehiking/?__a=1')
-    .then(response => response.json())
-    .then(console.log);
-  }, [])
+  // useEffect(() => {
+  //   fetch('https://instagram.com/illbehiking/?__a=1')
+  //   .then(response => response.json())
+  //   .then(console.log);
+  // }, [])
 
   return (
     <section className="photography">
