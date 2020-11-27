de238e64e52ddf9db6930c49d350e4c8400d66b4
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-26 17:33:15 -0800

    add modal which displays youtube video and information

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
-import ProjectDisplay from './ProjectDisplay';
+import { ReactIcon, JSIcon, RailsIcon, RubyIcon, ReduxIcon, CSSIcon, HTMLIcon, PSIcon } from './SkillsIcons';
+import Modal from "react-modal";
 
-const Project = ({ title, snippet }) => {
+Modal.setAppElement("#root");
+
+const Project = ({ title, snippet, url, languages }) => {
 
   const overlay = React.createRef();
   const line = React.createRef();
-  const [showProject, setShowProject] = useState(false);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+    handleButtonHover();
+  }
 
   const handleButtonHover = () => {
     if (overlay.current.className === "project-overlay-hover") {
@@ -17,17 +26,41 @@ const Project = ({ title, snippet }) => {
     }
   }
 
-  const handleClick = (event) => {
-    setShowProject(true)
-  }
+  const mapLanguages = () => {
+    if (languages) {
+      return languages.map(language => {
+        if (language === "react") {
+          return <ReactIcon />
+        } 
+        if (language === "javascript") {
+          return <JSIcon />
+        } 
+        if (language === "rails") {
+          return <RailsIcon />
+        } 
+        if (language === "ruby") {
+          return <RubyIcon />
+        } 
+        if (language === "redux") {
+          return <ReduxIcon />
+        } 
+        if (language === "css") {
+          return <CSSIcon />
+        } 
+        if (language === "html") {
+          return <HTMLIcon />
+        } 
+        if (language === "ps") {
+          return <PSIcon />
+        } 
+      })
 
-  const handleClose = () => {
-    setShowProject(false)
+    }
   }
 
   return (
     <div 
-      onClick={handleClick}
+      onClick={toggleModal}
       className="project-item" 
       onMouseEnter={handleButtonHover} 
       onMouseLeave={handleButtonHover} 
@@ -39,8 +72,40 @@ const Project = ({ title, snippet }) => {
         <p>{snippet}</p>
       </div>
       <div ref={overlay} className="project-overlay"></div>
-      {showProject && <ProjectDisplay display={true} closeModal={handleClose} />}
-  </div>
+
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={toggleModal}
+        contentLabel="My dialog"
+        className="modal"
+        overlayClassName="modal-overlay"
+        closeTimeoutMS={0}
+      >
+
+        <div className="modal-close" onClick={toggleModal}>x</div>
+        <div className="modal-content">
+          <div className="modal-column1">
+            <iframe width="420" height="315" src={url} title={title} frameBorder="0" allowFullScreen className="video" >
+            </iframe>
+          </div>
+          <div className="modal-column2">
+            <div className="modal-title">{title}</div>
+            {snippet}
+            {mapLanguages()}
+            <div className="view-code">
+              <a 
+                href="https://github.com/dougschallmoser/trailblaze-react-app" 
+                target="_blank" 
+                rel="noreferrer" 
+              >
+                VIEW CODE
+              </a>
+            </div>
+          </div>
+        </div>
+        
+      </Modal>
+    </div>
   )
 }
 
