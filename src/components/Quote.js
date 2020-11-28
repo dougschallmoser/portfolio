d02f9939859abd353065b26826ce1bfcc21aed78
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-27 19:19:56 -0800

    build component for section near footer

---
+++ src/components/Quote.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Quote = () => {
+  return (
+    <section id="quote">
+      <div className="quote-overlay"></div>
+    </section>
+  )
+}
+
+export default Quote;
