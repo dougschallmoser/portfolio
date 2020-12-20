8eba42916e5679a56506b2c8202870eb5b7f8e05
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-19 18:23:29 -0800

    update about text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -28,16 +28,22 @@ const About: React.FC = () => {
       <hr ref={hr} className="line" />
       <div className="about-content">
         <div id="about-mystory">
-          CIVIL TO SOFTWARE ENGINEER
+          BIO
         </div>
         <p>
-          Hello! My name is Doug Schallmoser and I am a software engineer living in Bellingham, WA. I love the challenge that coding brings and the subsequent potential it has to improve lives. My first experience with coding came in 2019 while I was studying for the PE (Professional Engineering) exam. I started watching lectures from Harvard's computer science course "CS50" in between studying for the exam. Before I knew it, I was hooked and wanted to pursue coding further.  
+          Hello! My name is Doug Schallmoser and I am a software engineer living in Bellingham, WA. I love the challenge that coding brings and the subsequent potential it has to improve lives. On a more personal level, I love the outdoors and when I'm not programming I'm typically hiking somewhere in the mountains. 
         </p>
+        <div id="about-mystory">
+          BACKGROUND IN CIVIL ENGINEERING
+        </div>
         <p>
-        I graduated from Florida Gulf Coast University in 2012 with Bachelor of Science degrees in Civil Engineering and Environmental Engineering. I went on to work for the Florida Department of Transportation as a civil engineer from late 2013 until early 2020. I learned many valuable things during this time, from technical engineering practices to project management skills. In January 2020, I became a licensed Professional Engineer in Florida. With this achievement complete, I made the decision to leave my job as I was overcome with a need to pursue a career more meaningful.
+        I graduated from Florida Gulf Coast University in 2012 with Bachelor of Science degrees in Civil Engineering and Environmental Engineering. I went on to work for the Florida Department of Transportation as a civil engineer from late 2013 until early 2020. I learned many valuable things during this time, from technical engineering practices to project management skills. In January 2020, I became a licensed Professional Engineer in Florida. With this achievement complete, I made the decision to leave my job as I was overcome with a need to pursue a career more meaningful to me.
         </p>
+        <div id="about-mystory">
+          TRANSITION TO SOFTWARE ENGINEERING
+        </div>
         <p>
-          After relocating to Washington with my wife, I enrolled in Flatiron School's Software Engineering program and formally began in July. I graduated the program in December 2020 having spent 1200+ hours learning and coding during the five-month long program. I built several projects in various programming languages to demonstrate my skills, all of which are provided on this site.
+          My first experience with coding came in 2019 where I began watching lectures from Harvard's computer science course "CS50" in between studying for the exam. Before I knew it, I was hooked and wanted to pursue coding further. After relocating to Washington with my wife, I enrolled in Flatiron School's Software Engineering program and formally began over the summer. I graduated the program in November 2020 having spent 1200+ hours learning and coding during the five-month long program.
         </p>
         <p>
         I am continuing my personal education efforts by building new projects and adding to my software knowledge base. My passion for coding combined with my technical background in engineering provides me with a unique skillset and perspective on problem-solving in this vast world of technology and programming.
