9156e75b96e093949ab7f7e2c1db3f539e05ff2c
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-20 07:55:41 -0800

    add section wrapper to return

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,19 +3,21 @@ import { ReactIcon, TSIcon, JSIcon, RailsIcon, RubyIcon, ReduxIcon, PostgresIcon
 
 const Skills: React.FC = () => {
   return (
-    <div className="about-icons">
-      <div id="about-skills">SKILLS THAT KEEP ME CREATIVE</div>
-      <ReactIcon cssName="about-icon" />
-      <TSIcon cssName="about-icon" />
-      <JSIcon cssName="about-icon" />
-      <RailsIcon cssName="about-icon" />
-      <RubyIcon cssName="about-icon" />
-      <ReduxIcon cssName="about-icon" />
-      <PostgresIcon cssName="about-icon" />
-      <CSSIcon cssName="about-icon" />
-      <HTMLIcon cssName="about-icon" />
-      <PSIcon cssName="about-icon" />
-    </div>
+    <section id="skills">
+      <div className="about-icons">
+        <div id="about-skills">SKILLS THAT KEEP ME CREATIVE</div>
+        <ReactIcon cssName="about-icon" />
+        <TSIcon cssName="about-icon" />
+        <JSIcon cssName="about-icon" />
+        <RailsIcon cssName="about-icon" />
+        <RubyIcon cssName="about-icon" />
+        <ReduxIcon cssName="about-icon" />
+        <PostgresIcon cssName="about-icon" />
+        <CSSIcon cssName="about-icon" />
+        <HTMLIcon cssName="about-icon" />
+        <PSIcon cssName="about-icon" />
+      </div>
+    </section>
   )
 }
 
