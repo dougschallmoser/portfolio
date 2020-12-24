ec3f92eb97e401ff7b6c62e3aa8823f7a1b838ea
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-23 20:16:05 -0800

    add div to splash section and swap id names

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -19,23 +19,25 @@ const Splash: React.FC = () => {
   }
 
   return (
-    <section id="splash">
-      <div className="splash-intro">
-        <div className="splash-name">DOUG SCHALLMOSER</div>
-        <hr ref={line} className="splash-line" />
-        <div className="splash-bio">
-          <div className="splash-title">
-            Software Engineer
-          </div>
-          Bellingham, WA
-          <div className="splash-button" onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>
-            <Link to="projects" smooth={true} offset={-58}>
-              VIEW PROJECTS
-            </Link>
+    <section id="splash-container">
+      <div id="splash">
+        <div className="splash-intro">
+          <div className="splash-name">DOUG SCHALLMOSER</div>
+          <hr ref={line} className="splash-line" />
+          <div className="splash-bio">
+            <div className="splash-title">
+              Software Engineer
+            </div>
+            Bellingham, WA
+            <div className="splash-button" onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>
+              <Link to="projects" smooth={true} offset={-58}>
+                VIEW PROJECTS
+              </Link>
+            </div>
           </div>
         </div>
+        <div ref={overlay} className="splash-overlay"></div>
       </div>
-      <div ref={overlay} className="splash-overlay"></div>
     </section>
   )
 }
