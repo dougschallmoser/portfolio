a771c045d3c23412d2764705932b4118d5c6bfe3
Doug Schallmoser <dougschallmoser@gmail.com>
2021-01-05 13:18:22 -0800

    implement click feature to conditionally render additional info in about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useLayoutEffect } from 'react';
+import React, { useRef, useLayoutEffect, useState } from 'react';
 
 const About: React.FC = () => {
 
   const hr = useRef<HTMLHRElement>(null);
+  const [clicked, setClicked] = useState<boolean>(false)
 
   useLayoutEffect(() => {
     if (hr.current) {
@@ -21,6 +22,10 @@ const About: React.FC = () => {
     
   }, [])
 
+  const handleClick = () => {
+    setClicked(!clicked)
+  }
+
   return (
     <section id="about">
       <div id="projects-header">MY STORY</div>
@@ -29,23 +34,29 @@ const About: React.FC = () => {
         <p>
           Hello! My name is Doug Schallmoser and I am a software engineer living in Bellingham, WA. I love the challenge that coding brings and the subsequent potential it has to improve lives. On a more personal level, I love the outdoors and when I'm not programming I'm typically hiking somewhere in the mountains. 
         </p>
-        <div id="about-mystory-container">
-          <div id="about-mystory">
-            CIVIL ENGINEER
+        {!clicked && <div id="about-more" onClick={handleClick}>READ MORE</div>}
+        {clicked ? 
+          <>
+            <div id="about-mystory-container">
+              <div id="about-mystory">
+                CIVIL ENGINEER
+                <p>
+                  I graduated from Florida Gulf Coast University in 2012 with BS degrees in Civil Engineering and Environmental Engineering. I went on to work for the Florida Department of Transportation as a civil engineer from late 2013 until early 2020. I learned many valuable things during this time, from technical engineering practices to project management skills. After obtaining my Professional Engineer (P.E) license, I made the decision to leave my job as I was overcome with a need to pursue a career more meaningful to me.
+                </p>
+              </div>
+              <div id="about-mystory">
+                SOFTWARE ENGINEER
+                <p>
+                  My first experience with coding came in 2019 where I began watching lectures from Harvard's computer science course "CS50" in between studying for the exam. Before I knew it, I was hooked and wanted to pursue coding further. After relocating to Washington with my wife, I enrolled in Flatiron School's Software Engineering program and formally began over the summer. I graduated the program in November 2020 having spent 1200+ hours learning and coding during the five-month long program.
+                </p>
+              </div>
+            </div>
+          
             <p>
-              I graduated from Florida Gulf Coast University in 2012 with BS degrees in Civil Engineering and Environmental Engineering. I went on to work for the Florida Department of Transportation as a civil engineer from late 2013 until early 2020. I learned many valuable things during this time, from technical engineering practices to project management skills. After obtaining my Professional Engineer (P.E) license, I made the decision to leave my job as I was overcome with a need to pursue a career more meaningful to me.
+            I am continuing my personal education efforts by building new projects and adding to my software knowledge base. My passion for coding combined with my technical background in engineering provides me with a unique skillset and perspective on problem-solving in this vast world of technology and programming.
             </p>
-          </div>
-          <div id="about-mystory">
-            SOFTWARE ENGINEER
-            <p>
-              My first experience with coding came in 2019 where I began watching lectures from Harvard's computer science course "CS50" in between studying for the exam. Before I knew it, I was hooked and wanted to pursue coding further. After relocating to Washington with my wife, I enrolled in Flatiron School's Software Engineering program and formally began over the summer. I graduated the program in November 2020 having spent 1200+ hours learning and coding during the five-month long program.
-            </p>
-          </div>
-        </div>
-        <p>
-        I am continuing my personal education efforts by building new projects and adding to my software knowledge base. My passion for coding combined with my technical background in engineering provides me with a unique skillset and perspective on problem-solving in this vast world of technology and programming.
-        </p>
+          </>
+        : null }
       </div>
     </section>
   )
