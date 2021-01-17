e7bd8f64a4f7de3796978dfdce49999e621b5dfe
Doug Schallmoser <dougschallmoser@gmail.com>
2021-01-16 16:21:00 -0800

    implement redirecting for any path other than root path

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
@@ -8,6 +8,7 @@ ReactDOM.render(
     <Route exact path="/">
       <App />
     </Route>
+    <Route render={() => <Redirect to="/" />} />
   </Router>,
   document.querySelector('#root')
 );
